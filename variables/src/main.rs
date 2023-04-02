33b702510560e637ce3ee1e458b32855b4e7f30b
James Manning <jamesmanningr98@gmail.com>
2023-04-02 23:36:16 +0100

    Added an example of a const

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
+    // Adding a const, This cannot be computed at runtime as it is computed on compilation
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
     // Adding mut now will allow the variable to be re-assigned a value
     let mut x = 5;
     println!("The value of x is: {x}");
