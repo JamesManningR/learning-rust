fe5532334d1fe016afcdd7e072506af29166857c
James Manning <jamesmanningr98@gmail.com>
2023-04-02 21:47:31 +0100

    Added rng

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,14 @@
 // Import the io module from the 'std' lib
+use rand::Rng;
 use std::io;
 
 // This is a special function which runs at the start of the program
 fn main() {
     println!("Guess the numebr!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Secret number: {secret_number}");
 
     println!("Please input your guess:");
 
@@ -15,5 +20,5 @@ fn main() {
         .read_line(&mut guess) // Read the line in the terminal
         .expect("Failed to read line"); // Handle a situation where the function fails and output an error messege of 'failed to read line'
 
-        println!("You guessed: {guess}"); // Print out the guess, crab pincers '{}' open an expression
+    println!("You guessed: {guess}"); // Print out the guess, crab pincers '{}' open an expression
 }
