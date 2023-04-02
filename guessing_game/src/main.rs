800e78e766874d37244866631969526aeff13868
James Manning <jamesmanningr98@gmail.com>
2023-04-02 22:13:32 +0100

    Added guess number parser

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 // Import the io module from the 'std' lib
 use rand::Rng;
 use std::io;
+use std::cmp::Ordering;
 
 // This is a special function which runs at the start of the program
 fn main() {
@@ -20,5 +21,16 @@ fn main() {
         .read_line(&mut guess) // Read the line in the terminal
         .expect("Failed to read line"); // Handle a situation where the function fails and output an error messege of 'failed to read line'
 
+    let guess: u32 = guess // Create a new variable called guess (Shadowing)
+        .trim() // Trim the originals whitespace
+        .parse() // Parse into a number
+        .expect("Please type a number"); // If it's not a number, then it's invalid
+
     println!("You guessed: {guess}"); // Print out the guess, crab pincers '{}' open an expression
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too Large"),
+        Ordering::Equal => println!("Just Right"),
+    }
 }
