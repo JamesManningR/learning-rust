00ed236f7293434d5fdc1a81df1bd11dd9d9a2e3
James Manning <jamesmanningr98@gmail.com>
2023-04-02 23:22:36 +0100

    added unworkable code

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    // Setting up x without the mut keyword
+    let x = 5;
+    println!("The value of x is: {x}");
+
+    // Re-assigning will produce an error
+    x = 6;
+    println!("The value of x is: {x}");
 }
