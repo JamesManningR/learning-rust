afa3d9c639f9063af5fcbc9cc1d8e27d916d4b62
James Manning <jamesmanningr98@gmail.com>
2023-04-02 22:15:37 +0100

    added end loop and winning case

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,26 +11,31 @@ fn main() {
 
     println!("Secret number: {secret_number}");
 
-    println!("Please input your guess:");
+    loop {
+        println!("Please input your guess:");
 
-    // Create a mutable variable called guess, which is a string
-    let mut guess = String::new();
+        // Create a mutable variable called guess, which is a string
+        let mut guess = String::new();
 
-    // Standart input from terminal
-    io::stdin()
-        .read_line(&mut guess) // Read the line in the terminal
-        .expect("Failed to read line"); // Handle a situation where the function fails and output an error messege of 'failed to read line'
+        // Standart input from terminal
+        io::stdin()
+            .read_line(&mut guess) // Read the line in the terminal
+            .expect("Failed to read line"); // Handle a situation where the function fails and output an error messege of 'failed to read line'
 
-    let guess: u32 = guess // Create a new variable called guess (Shadowing)
-        .trim() // Trim the originals whitespace
-        .parse() // Parse into a number
-        .expect("Please type a number"); // If it's not a number, then it's invalid
+        let guess: u32 = guess // Create a new variable called guess (Shadowing)
+            .trim() // Trim the originals whitespace
+            .parse() // Parse into a number
+            .expect("Please type a number"); // If it's not a number, then it's invalid
 
-    println!("You guessed: {guess}"); // Print out the guess, crab pincers '{}' open an expression
+        println!("You guessed: {guess}"); // Print out the guess, crab pincers '{}' open an expression
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small"),
-        Ordering::Greater => println!("Too Large"),
-        Ordering::Equal => println!("Just Right"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Large"),
+            Ordering::Equal => {
+                println!("Just Right")
+                break;
+            },
+        }
     }
 }
