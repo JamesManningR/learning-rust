f21ee8b5a1700d8a809225ab4ece79891eea2af8
James Manning <jamesmanningr98@gmail.com>
2023-04-02 23:44:02 +0100

    added examples of shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     // Adding a const, This cannot be computed at runtime as it is computed on compilation
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("3hr in secs: {THREE_HOURS_IN_SECONDS}");
 
     // Adding mut now will allow the variable to be re-assigned a value
     let mut x = 5;
@@ -9,4 +10,23 @@ fn main() {
     // This will no longer produce an error
     x = 6;
     println!("The value of x is: {x}");
+
+    let x = x + 6;
+    println!("The value of x is: {x}");
+    
+    // Example of shadowing
+    let x = "Pringles";
+    println!("The value of x is: {x}");
+
+    // Example of shadowing
+    let x = 12;
+    println!("The value of x is: {x}");
+    
+    {
+        let x = "Shadowing in a scope?";
+        println!("The value of x in this scope is: {x}");
+    }
+
+    println!("Oh look, now back in this scope: {x}");
+
 }
