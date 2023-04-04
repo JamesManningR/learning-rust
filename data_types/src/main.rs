55eb8b6106c69a8a61a8e6e4b2127fc8c63d3cd6
James Manning <jamesmanningr98@gmail.com>
2023-04-04 10:38:41 +0100

    Added function examples and formatted

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,36 +1,36 @@
 fn main() {
     // Scalar types ========================================
     // Integers
-    let intDefault = 5; // Signed 32 bit integer (rust defaults to i32 if no type is specified)
+    let int_default = 5; // Signed 32 bit integer (rust defaults to i32 if no type is specified)
 
     let int8: i8 = 5; // Signed 8 bit integer
-    let intUnsigned8: u8 = 5; // Unsigned 8 bit integer
+    let int_unsigned8: u8 = 5; // Unsigned 8 bit integer
 
     let int32: i32 = 5; // Signed 32 bit integer
-    let intUnsigned32: u32 = 5; // Unsigned 32 bit integer (32 bit intager)
+    let int_unsigned32: u32 = 5; // Unsigned 32 bit integer (32 bit intager)
 
     let int64: i64 = 5; // Signed 64 bit integer
-    let intUnsigned64: u64 = 5; // Unsigned 64 bit integer
+    let int_unsigned64: u64 = 5; // Unsigned 64 bit integer
 
     let int128: i128 = 5; // Signed 128 bit integer
-    let intUnsigned128: u128 = 5; // Unsigned 128 bit integer
+    let int_unsigned128: u128 = 5; // Unsigned 128 bit integer
 
-    let intArch: isize = 5; // Signed integer, the size of the pointer
-    let intUnsignedArch: usize = 5; // Unsigned integer, the size of the pointer
+    let int_arch: isize = 5; // Signed integer, the size of the pointer
+    let int_unsigned_arch: usize = 5; // Unsigned integer, the size of the pointer
 
     // Floating point
-    let floatDefault = 5.0; // 64 bit floating point number (rust defaults to f64 if no type is specified)
+    let float_default = 5.0; // 64 bit floating point number (rust defaults to f64 if no type is specified)
 
     let float32: f32 = 5.0; // 32 bit floating point number
     let float64: f64 = 5.0; // 64 bit floating point number
 
     // Boolean
-    let boolTrue = true;
-    let boolFalse = false;
+    let bool_true = true;
+    let bool_false = false;
 
     // Characters (supports unicode)
     let char = 'a'; // Single character
-    let charEmoji = '😻'; // Single character
+    let char_emoji = '😻'; // Single character
 
     // Compound types ========================================
     // Tuples
@@ -38,14 +38,38 @@ fn main() {
     let (x, y, z) = tup; // Destructuring a tuple
     println!("The value of y is: {y}"); // 6.4
 
-    let fiveHundred = tup.0; // Accessing a tuple element
-    let sixPointFour = tup.1; // Accessing a tuple element
+    let five_hundred = tup.0; // Accessing a tuple element
+    let six_point_four = tup.1; // Accessing a tuple element
     let one = tup.2; // Accessing a tuple element
 
     // Arrays
     let arr = [1, 2, 3, 4, 5]; // Arrays are fixed length
     let first = arr[0]; // Accessing an array element
     let second = arr[1]; // Accessing an array element
-    let typedArr: [i32; 5] = [1, 2, 3, 4, 5]; // Arrays can be typed
-    let allZeros = [0; 5]; // Arrays can be initialized with a value
+    let typed_arr: [i32; 5] = [1, 2, 3, 4, 5]; // Arrays can be typed
+    let all_zeros = [0; 5]; // Arrays can be initialized with a value
+
+    // Function
+    your_first_non_main_function();
+    function_with_param(11);
+    function_with_param(5);
+    function_with_multiple_params(21, 'm')
+}
+
+fn your_first_non_main_function() {
+    println!("Hello, world!");
+}
+
+// Function params must be typed
+fn function_with_param(x: i32) {
+    println!("The value of x is {x}");
+}
+
+// Function params must be typed
+fn function_with_multiple_params(value: i32, label: char) {
+    println!("{label}: {value}");
+}
+
+fn expression(num: i32) -> i32 {
+    num * num
 }
