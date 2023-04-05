0144653b4fc82ad9a19aa066ff0d34482c734524
James Manning <jamesmanningr98@gmail.com>
2023-04-05 16:28:25 +0100

    Added ref example

--- core_concepts/ownership/src/main.rs
+++ core_concepts/ownership/src/main.rs
@@ -8,7 +8,14 @@ fn main() {
     // takes ownership of the s variable.
     // we can get around this by transferring ownership back by assigning it to another variable
     let s2 = String::from("Hi");
-    let s3 = print_my_string_and_return(s2);
+    let _s3 = print_my_string_and_return(s2);
+
+    // We can also instead use references rather than the values themselves
+    let str_for_ref = String::from("Memes");
+    print_my_string_ref(&str_for_ref);
+    // As you can see, we can use it again without any issues
+    println!("{str_for_ref}");
+
 
     let x = 5;
 
@@ -23,6 +30,13 @@ fn print_my_string_and_return(a_string: String) -> String {
     println!("{a_string}");
 
     a_string
+}
+
+fn print_my_string_ref(a_string: &String) {
+    println!("{a_string}");
+    // Note we cannot mutate this in its current state
+    // This would throw a error
+    // a_string.push_str(" lmao");
 }
 
 fn print_my_int(an_int: i32) {
