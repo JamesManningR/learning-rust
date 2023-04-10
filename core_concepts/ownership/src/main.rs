667768383cdd97786f4407791b6b217b358592d1
James Manning <jamesmanningr98@gmail.com>
2023-04-10 22:44:44 +0100

    added mutable reference example

--- core_concepts/ownership/src/main.rs
+++ core_concepts/ownership/src/main.rs
@@ -16,10 +16,16 @@ fn main() {
     // As you can see, we can use it again without any issues
     println!("{str_for_ref}");
 
-
     let x = 5;
 
     print_my_int(x);
+
+    let mut mutable_string = String::from("Rust memes are hard to find");
+
+    // If we want to use mutable references, then we need to explicitly define this behaviour
+    // we do this by providing the &mut to the argumnet as well as in the function
+    mutate_my_string(&mut mutable_string);
+    println!("{mutable_string}");
 }
 
 fn print_my_string(a_string: String) {
@@ -37,6 +43,11 @@ fn print_my_string_ref(a_string: &String) {
     // Note we cannot mutate this in its current state
     // This would throw a error
     // a_string.push_str(" lmao");
+}
+
+// Not this function can access and mutate the referenced variable
+fn mutate_my_string(a_string: &mut String) {
+    a_string.push_str(" because of the video game");
 }
 
 fn print_my_int(an_int: i32) {
