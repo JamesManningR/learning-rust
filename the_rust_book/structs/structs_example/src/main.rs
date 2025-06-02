24b4df80618f36600b982cbb776cada6e2d0f589
JamesManningR <56916766+JamesManningR@users.noreply.github.com>
2025-06-02 20:30:52 +0200

    mid way through structs

--- the_rust_book/structs/structs_example/src/main.rs
+++ the_rust_book/structs/structs_example/src/main.rs
@@ -1,13 +1,39 @@
+// By default structs do not implement the ability to print in their entirity
+#[derive(Debug)] // So we have to opt in
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn set_width(&mut self, width: u32) {
+        self.width = width
+    }
+    fn set_height(&mut self, height: u32) {
+        self.height = height
+    }
+}
+
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let mut rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("Debug Rectangle: {rect:#?}"); // :? prints the strucy # makes it pretty
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        rect.area()
     );
-}
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+    rect.set_width(5);
+    rect.set_height(3);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect.area()
+    );
 }
